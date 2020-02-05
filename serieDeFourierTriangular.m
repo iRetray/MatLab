2d9be8ff9dfe47db3ce8ecf478f28f4b2ccbd05b
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-02-05 10:01:16 -0500

    Terminado ejercicio de Fourier triangular

--- serieDeFourierTriangular.m
+++ serieDeFourierTriangular.m
@@ -3,26 +3,37 @@ clear global
 close all
 
 % ENTRADAS
+amplitud = 10;
 frecuencia = 10e3;
-repeticiones = 1; % numero impar
+repeticiones = 1;
 resolucion = 1000;
-cantidadPeriodos = 2;
+cantidadPeriodos = 5;
+cantidadFrecuencia = 30;
+triangular = 0;
 
 % PROCESOS
 tiempo = linspace(0,cantidadPeriodos/frecuencia,resolucion);
 
-for coeficiente = 1.0:+2.0:repeticiones
-    componente = (1/coeficiente)*sin(2*pi*coeficiente*frecuencia*tiempo);
-    if coeficiente == 1
-        cuadrada = componente;
-    else
-       cuadrada = cuadrada + componente; 
-    end       
+coeficenteDeAmplitud = (pi/2) - (4/pi);
+sumatoriaUnica = cos((2*n-1)*w) / ((2*n-1)*(2*n-1));
+
+for coeficiente = 1.0:+2.0:(cantidadFrecuencia*2-1)
+    triangular = triangular + (1/(coeficiente^2)) * cos(2*pi*coeficiente*frecuencia*tiempo);     
 end
 
-texto = cat(2,'Resultante serie de Fourier hasta: ', num2str(repeticiones));
+triangularCompleta = (pi/2)-(4/pi) * triangular; 
+
+texto = cat(2,'Resultante serie de Fourier Triangular hasta: ', num2str(repeticiones));
+texto2 = cat(2,'Resultante serie de Fourier Triangular con Coeficiente hasta: ', num2str(repeticiones));
 
 figure(1)
-plot(tiempo,cuadrada)
+subplot(2,1,1)
+plot(tiempo,triangular)
 title(texto)
+grid on
+
+figure(1)
+subplot(2,1,2)
+plot(tiempo,triangularCompleta)
+title(texto2)
 grid on
