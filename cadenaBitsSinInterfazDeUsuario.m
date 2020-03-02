d8a10e94bb9dfbaea65cf7437680a1cf277e0087
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-03-02 11:14:24 -0500

    Creacion de appDesign para el codificador de señales

--- cadenaBitsSinInterfazDeUsuario.m
+++ cadenaBitsSinInterfazDeUsuario.m
@@ -1,5 +1,5 @@
 clc
-clear all 
+clear global 
 close all
 %---INPUT---
 texto = 'a';
@@ -36,31 +36,16 @@ if (regla_bit_alto ==1)
     cadena_bits = 5*frame;
  
 else 
-    cadena_bits = 5*frameinv; 
+    cadena_bits = 10*frameinv; 
     
 end
 
 
-
-
 %---OUTPUT---
 
-fprintf('texto: %s\n',texto)
-ascii
-bits
-cadena
 
 figure (1)
-subplot (2,1,1)
-plot(frame)
-axis ([ 0 length(frame) -1 6 ])
-title('señal binaria')
-grid on
-
-
-figure (1)
-subplot (2,1,2)
 plot(cadena_bits)
-axis ([ 0 length(cadena_bits) -1 6 ])
+axis ([ 0 length(cadena_bits) -1 11 ])
 title('cadena BITS')
 grid on
